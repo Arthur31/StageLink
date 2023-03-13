294c70cf93e431b3ac4b4faf43027c25aa87aefa
Arthur Marty <arthurmarty31@gmail.com>
2023-03-13 09:19:04 +0100

    Forward chat mesage to socket

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ const express = require('express');
 import { Express, Request, Response } from 'express';
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import { LiveChat } from "youtube-chat"
+import { MessageText } from "youtube-chat/dist/types/yt-response";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
+
+const channelId = "UCSJ4gkVC6NrvII8umztf0Ow" // Lofi Girl
+const liveChat = new LiveChat({ channelId: channelId })
 
 const port = 3000;
 
@@ -16,16 +21,58 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/onAir', (req: Request, res: Response) => {
-    io.sockets.emit("onAir", { onAir: true})
+    io.sockets.emit("onAir", { onAir: true })
     res.send('OnAir');
 });
 
 app.get('/offAir', (req: Request, res: Response) => {
-    io.sockets.emit("onAir", { onAir: false})
+    io.sockets.emit("onAir", { onAir: false })
     res.send('Off Air');
 });
 
-  
+
+
+liveChat.on("start", (liveId) => {
+    /* Your code here! */
+    console.log("liveChat.on(start)");
+    console.log(liveId);
+    
+    
+})
+
+// Emit at end of observation chat.
+// reason: string?
+liveChat.on("end", (reason) => {
+    /* Your code here! */
+})
+
+// Emit at receive chat.
+// chat: ChatItem
+liveChat.on("chat", (chatItem) => {
+    /* Your code here! */
+
+    let message = chatItem.message[0] as MessageText
+    console.log(message.text);
+
+    
+
+    io.sockets.emit("chat", {
+         msg: message.text,
+         authorName: chatItem.author.name,
+         authorThumbnail: chatItem.author.thumbnail?.url,
+        })
+    
+    
+})
+
+// Emit when an error occurs
+// err: Error or any
+liveChat.on("error", (err) => {
+    /* Your code here! */
+})
+
+
+
 io.on("connection", (socket: Socket) => {
     connection.push(socket);
     console.log("new conneciton");
@@ -33,17 +80,26 @@ io.on("connection", (socket: Socket) => {
     socket.on('disconnect', (data) => {
         connection.splice(connection.indexOf(socket), 1);
         console.log("Disconnect");
-        
+
     })
 
     socket.on('srv', (data) => {
         console.log(data);
-        io.sockets.emit("msg", {msg: "salut du server"})
-        
+        io.sockets.emit("msg", { msg: "salut du server" })
+
     })
-    
+
 });
 
-httpServer.listen(port, ()=> {
+async function main() {
+    const ok = await liveChat.start()
+    if (!ok) {
+        console.log("Failed to start, check emitted error")
+    }
+}
+
+
+main()
+httpServer.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
